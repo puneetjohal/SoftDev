07e3c648c320402776dcf807d9624c6823567158
Puneet <pjohal@stuy.edu>
2018-09-25 21:24:39 -0400

    updated according to code review

--- 10_occupy_flask_st/occ.py
+++ 10_occupy_flask_st/occ.py
@@ -8,29 +8,28 @@ import random
 
 app = Flask(__name__)
 
-#parse through the file
+#parse through the csv file
 def parse(inputfile):
     file = open(inputfile, 'r')  #open the file in read mode
     raw = file.read()           #get the text
-    return raw.split("\n")      #split on new lines
+    return raw.split("\n")      #split on new lines + return list of lines
 
 #separate jobs from percentages
 def separate(inputlist):
-    cnt = 0 #counter
-    while cnt < len(inputlist):
-        if '"' in inputlist[cnt]:
-            inputlist[cnt] = inputlist[cnt].replace('"', '')
-            inputlist[cnt] = inputlist[cnt].rsplit(',', 1)
-            cnt += 1
+    index = 0
+    while index < len(inputlist):
+        if '"' in inputlist[index]:
+            inputlist[index] = inputlist[index].replace('"', '') #remove extra quotes
+        inputlist[index] = inputlist[index].rsplit(',', 1) #split at last comma in line (the one before the percetage)
+        index += 1
 
 #feed info into dictionary
-def listToDict(inputlist)
+def listToDict(inputlist):
     jobs = {}
-    cnt = 1 #counter
-    while cnt < len(inputlist) - 2:
-        jobs[inputlist[cnt][0]] = float(inputlist[cnt][1]) #Each even index element (job) becomes a key, 
-        #and the odd index element right after it becomes the value
-        cnt += 1
+    line = 1 #skips table header
+    while line < (len(inputlist) - 2):
+        jobs[inputlist[line][0]] = float(inputlist[line][1]) #Each even index element (job) becomes a key, and the odd index element right after it becomes the value
+        line += 1
     return jobs
 
 #random job selection
@@ -38,14 +37,22 @@ def randomJob(dictionary):
     keys = list(dictionary) #returns list of all keys in dict
     return random.choice(keys)
 
+
+@app.route("/")
+def redirect():
+    return 'Add "/occupations" to url'
+
 @app.route("/occupations")
 def display():
+  lines = parse('data/occupations.csv')
+  separate(lines)
+  jobs = listToDict(lines)
   return render_template("seed.html",
-						 title = 'random occupation generator',
+						 title = 'Random occupation generator',
 						 head0 = 'Job Class',
                          head1 = 'Percentage',
-						 dict = dict,
-						 val = randomJob())
+						 dict = jobs,
+						 val = randomJob(jobs))
 
 if __name__ == "__main__":
     app.debug == True
