1efc5641f10bec3f4b30bd63ba5ca2fa2de32d65
Puneet <pjohal@stuy.edu>
2018-09-28 06:11:47 -0400

    13

--- 13_formation/app.py
+++ 13_formation/app.py
@@ -1,11 +1,25 @@
+#Puneet Johal
+#SoftDev1 pd7
+#K13 -- Echo Echo Echo . . .
+#2018-09-28
 from flask import Flask
 from flask import render_template
+from flask import request
 
 app = Flask(__name__)
 
 @app.route("/")
-def func():
-    return render_template("woo.html")
+def redirect():
+    return render_template ('woo.html')
+
+@app.route("/auth")
+def authenticate():
+    print(app)
+    print(request)
+    print(request.args)
+    name = request.args['username']
+    meth = request.method
+    return '<h3> Have a blessed day, ' + name + '</h3> <br> Request method used: ' + meth
 
 if __name__ == "__main__":
     app.debug == True
