eabf4e1ffae35c3cc25f652b1b92fe751bda1e14
Puneet <pjohal@stuy.edu>
2018-11-14 05:25:33 -0500

    k24

--- 24_rest/app.py
+++ 24_rest/app.py
@@ -1,10 +1,18 @@
-import Flask, render_template, json, urllib
+import json, urllib.request, urllib.parse
+from flask import Flask, render_template
+app = Flask(__name__)
 
 @app.route("/")
-def root():
-    u = urllib.urlopen("link")
+def func():
+    u = urllib.request.urlopen("https://api.nasa.gov/planetary/apod?api_key=MrEGd06mQpt76yu7K4MRWHdg47cqQCG3jl6d0QHO")
     response = u.read()
     data = json.loads(response)
+    #print(data)
+    return render_template("index.html",
+                           img = data['url'],
+                           explain = data['explanation'],
+                           allmeta = data)
+
 
 
 if __name__ == "__main__":
